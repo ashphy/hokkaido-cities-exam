afefcf62e8da4846daf185f5768193f58267480d
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2024-01-21 12:54:38 +0900

    SNSシェア機能

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -8,6 +8,9 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { MastodonShare } from "@/features/share/MastodonShare";
+import { MisskeyShare } from "@/features/share/MisskeyShare";
+import { XShare } from "@/features/share/XShare";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { useAtomValue, useSetAtom } from "jotai";
 import { Button } from "./ui/button";
@@ -18,6 +21,8 @@ export const Answer = () => {
 
   const numberOfCities = cities.length;
   const numberOfCorrectAnswers = useAtomValue(numOfCorrectAtom);
+
+  const shareText = `#北海道市町村テスト で${numberOfCities}市町村中「${numberOfCorrectAnswers}」市町村正解しました！`;
 
   return (
     <Dialog
@@ -30,22 +35,28 @@ export const Answer = () => {
           <Button className="w-full">答え合わせ</Button>
         </div>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-w-screen-md">
         <DialogHeader>
           <DialogTitle>北海道市町村テスト 答え合わせ</DialogTitle>
           <DialogDescription>
-            <div className="text-center">
-              <span className="text-9xl text-red-800">
-                {numberOfCorrectAnswers}
-              </span>
-              <span className="text-6xl"> / {numberOfCities}</span>
+            <div className="flex flex-col items-center">
+              <div>
+                <span className="text-9xl text-red-800">
+                  {numberOfCorrectAnswers}
+                </span>
+                <span className="text-6xl"> / {numberOfCities}</span>
+              </div>
               <span className="text-xl"> 市町村 正解！</span>
             </div>
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <DialogClose asChild>
-            <Button type="button">正解を見る</Button>
+          <DialogClose className="w-full" asChild>
+            <div className="flex flex-row justify-between">
+              <MastodonShare text={shareText} />
+              <MisskeyShare text={shareText} />
+              <XShare text={shareText} />
+            </div>
           </DialogClose>
         </DialogFooter>
       </DialogContent>
