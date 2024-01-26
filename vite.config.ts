452b391b2cc8c4e7aa4c9b392d34100b6fd9c41a
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2024-01-27 00:46:04 +0900

    github actions build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://ashphy.github.io/hokkaido-cities-exam/",
   plugins: [react()],
   resolve: {
     alias: {
