d460e4bed7a6d353d68302c4694d01d4033a7f17
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2024-01-27 00:47:41 +0900

    fix lint

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { Eraser } from "lucide-react";
 import { useCallback } from "react";
 import {
